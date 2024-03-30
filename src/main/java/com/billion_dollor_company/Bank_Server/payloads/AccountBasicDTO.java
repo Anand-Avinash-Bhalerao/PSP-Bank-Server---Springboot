a1b571c73feaae8ec4db1bb21892b144e5934feb
Anand Avinash Bhalerao <anandbhalerao.l.a3b@gmail.com>
2024-03-30 18:48:09 +0530

    Added function for mapping AccountBasicProjection.java to AccountBasicDTO.java

--- src/main/java/com/billion_dollor_company/Bank_Server/payloads/AccountBasicDTO.java
+++ src/main/java/com/billion_dollor_company/Bank_Server/payloads/AccountBasicDTO.java
@@ -1,10 +1,44 @@
 package com.billion_dollor_company.Bank_Server.payloads;
 
-public interface AccountBasicDTO {
-    String getUpiID();
-    String getFirstName();
-    String getMiddleName();
-    String getLastName();
-    String getAccountNo();
-    String getMobileNo();
+//public interface AccountBasicDTO {
+//    String getUpiID();
+//    String getFirstName();
+//    String getMiddleName();
+//    String getLastName();
+//    String getAccountNo();
+//    String getMobileNo();
+//}
+
+import jakarta.persistence.Column;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class AccountBasicDTO{
+    private String upiID;
+
+    private String firstName;
+
+    private String middleName;
+
+    private String lastName;
+
+    private String accountNo;
+
+    private String mobileNo;
+
+    public AccountBasicDTO(AccountBasicProjection projection) {
+        this.upiID = projection.getUpiID();
+        this.firstName = projection.getFirstName();
+        this.middleName = projection.getMiddleName();
+        this.lastName = projection.getLastName();
+        this.accountNo = projection.getAccountNo();
+        this.mobileNo = projection.getMobileNo();
+    }
 }
+
