3eb9f20f28c181ee82ba5924a7f84632a01da389
Anand Avinash Bhalerao <anandbhalerao.l.a3b@gmail.com>
2024-03-30 17:13:00 +0530

    Renamed the file

---
+++ src/main/java/com/billion_dollor_company/Bank_Server/repository/AccountInfoRepository.java
@@ -0,0 +1,31 @@
+package com.billion_dollor_company.Bank_Server.repository;
+
+import com.billion_dollor_company.Bank_Server.payloads.AccountBasicInfo;
+import com.billion_dollor_company.Bank_Server.models.AccountInfo;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
+@Repository
+public interface AccountInfoRepository extends JpaRepository<AccountInfo, Integer> {
+
+    // This fetches only the required fields from the database.
+    @Query("SELECT a.upiID as upiID, a.firstName as firstName, a.middleName as middleName, a.lastName as lastName, a.accountNo as accountNo, a.mobileNo as mobileNo FROM AccountInfo a WHERE a.upiID = :upiID")
+    AccountBasicInfo getByUpiID(@Param("upiID") String upiID);
+
+    // Returns the entire record of a user. It contains: firstName, middleName, lastName, mobileNo, upiID, balance, accountNo
+    AccountInfo findByUpiID(String upiID);
+
+    // Used when a transaction is being made, and we need to update the bank balance.
+    // It returns an int which indicates how many rows were affected.
+    @Transactional
+    @Modifying
+    @Query(
+            value = "update AccountInfo a set a.balance = :balance where a.upiID = :upiID"
+    )
+    int updateBalance(@Param("balance") String balance, @Param("upiID") String upiID);
+
+}
