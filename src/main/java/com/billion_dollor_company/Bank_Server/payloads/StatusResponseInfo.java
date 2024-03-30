cc4ec417449a8b9acd3c4a912c76bd328f48e8bf
Anand Avinash Bhalerao <anandbhalerao.l.a3b@gmail.com>
2024-03-30 17:12:43 +0530

    For handling the response sent back to the user in case of failed operations

---
+++ src/main/java/com/billion_dollor_company/Bank_Server/payloads/StatusResponseInfo.java
@@ -0,0 +1,15 @@
+package com.billion_dollor_company.Bank_Server.payloads;
+
+import com.fasterxml.jackson.dataformat.xml.annotation.JacksonXmlRootElement;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@JacksonXmlRootElement(localName = "status")
+public class StatusResponseInfo {
+    public String status;
+    public String message;
+}
